e1329d55b6ffba9ea56169f4eb6e8ca5a28a81a4
Travis Collins <travis@tec27.com>
2024-03-26 02:09:24 -0700

    Don't trigger transform change detection for things that aren't actually changes.

--- src/render/ysort.rs
+++ src/render/ysort.rs
@@ -12,6 +12,9 @@ pub fn y_sort(mut query: Query<(&mut Transform, &YSort)>) {
     // Formula adjusted to better work with the range of Y values BW positions have.
     for (mut transform, ysort) in query.iter_mut() {
         let y = transform.translation.y;
-        transform.translation.z = ysort.0 - (1.0f32 / (1.0f32 + 2.0f32.powf(-0.0001 * y)));
+        let new_z = ysort.0 - (1.0f32 / (1.0f32 + 2.0f32.powf(-0.0001 * y)));
+        if new_z != transform.translation.z {
+            transform.translation.z = new_z;
+        }
     }
 }
